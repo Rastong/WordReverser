3b1a578dffde6f28500609138f8814393502e88d
Raston Gilbert <rastong@gmail.com>
2021-10-07 16:45:28 -0400

    first commit

---
+++ reverse words/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace reverse_words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a word");
+            string input = Console.ReadLine();
+            ReverseWord(input);
+        }
+
+
+        public static void ReverseWord(string input)
+        {
+            string outcome = "";
+            Stack<String> reverse = new Stack<string>();
+            int i = 0;
+            for (i = 0; i < input.Length; i++)
+            {
+                reverse.Push(input.Substring(i, 1));
+            }
+
+            foreach (string letter in reverse)
+            {
+                outcome += letter + ",";
+            }
+            outcome = outcome.Remove(outcome.LastIndexOf(","));
+            Console.WriteLine(outcome + ".");
+        }
+    }
+}
